c788fbb39dbd8b4e254361594a59bf307bd77b4e
Clinton J Wang <clintonjwang@gmail.com>
2022-06-07 00:32:02 -0400

    et

---
+++ inrnet/experiments/autoencode.py
@@ -0,0 +1,3 @@
+
+def pretrain_inrnet():
+	return
---
+++ inrnet/pretrain.py
@@ -0,0 +1,25 @@
+"""
+Entrypoint for pretraining
+"""
+import torch, wandb
+import numpy as np
+from functools import partial
+
+from inrnet import args as args_module
+from inrnet.experiments.autoencode import pretrain_inrnet
+
+def main():
+    args = args_module.parse_args()
+    if not torch.cuda.is_available():
+        raise ValueError("cuda is not available on this device")
+    torch.backends.cudnn.benchmark = True
+    if args["random seed"] >= 0:
+        np.random.seed(args["random seed"])
+        torch.manual_seed(args["random seed"])
+    if args['sweep_id'] is not None:
+        wandb.agent(args['sweep_id'], function=partial(pretrain_inrnet, args=args), count=1, project='inrnet')
+    else:
+        pretrain_inrnet(args=args)
+
+if __name__ == "__main__":
+    main()
