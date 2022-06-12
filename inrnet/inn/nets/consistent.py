47d782e262c54875876ba0c37bb0bfc168a382f3
clintonjwang <clintonjwang@gmail.com>
2022-06-12 15:03:23 -0400

    b

--- inrnet/inn/nets/consistent.py
+++ inrnet/inn/nets/consistent.py
@@ -4,20 +4,32 @@ from inrnet.inn.inr import DiscretizedINR
 from inrnet.inn.layers.other import FlowLayer
 from inrnet.inn.nets.inrnet import INRNet
 
-class ConsistentNet(INRNet):
-    def __init__(self, dense_sampler, flow_layers: nn.Module, ratio=8, **kwargs):
+class LearnedSampler(INRNet):
+    def __init__(self, dense_sampler, query_layers: nn.Module,
+        flow_layers: nn.Module, ratio=16, **kwargs):
         """_summary_
 
         Args:
             dense_sampler (_type_): full QMC sampler
-            layers (nn.Module): _description_
+            query_layers (nn.Module): layers whose output should be preserved under
+                            dense v. sparse sampling
+            flow_layers (nn.Module): layers to estimate sampling pattern
+            ratio (int, optional): ratio of points in dense to sparse sampler. Defaults to 16.
         """        
-        super().__init__(sampler=dense_sampler, layers=flow_layers, **kwargs)
-        sparse_sampler = dense_sampler
-        sparse_sampler['sample points'] = dense_sampler['sample points']//ratio
+        super().__init__(sampler=None, layers=None, **kwargs)
+        self.flow_layers = flow_layers
+        self.query_layers = query_layers
+        self.dense_sampler = dense_sampler.copy()
+        self.sparse_sampler = dense_sampler
+        self.sparse_sampler['sample points'] = dense_sampler['sample points']//ratio
         
     def forward(self, inr: DiscretizedINR):
-        return
+        dense_inr = self.sample_inr(inr, self.dense_sampler)
+        sparse_inr = self.sample_inr(inr, self.sparse_sampler)
+        flow = self.flow_layers(sparse_inr)
+        sparse_inr.coords = sparse_inr.coords + flow.values
+        return self.query_layers(sparse_inr), self.query_layers(dense_inr)
 
-    def loss(self, sampler):
-        return
+    def loss(self, inr: DiscretizedINR):
+        sparse, dense = self.forward(inr)
+        return (sparse - dense).abs().sum()
--- tests/test_util.py
+++ tests/test_util.py
@@ -5,14 +5,5 @@ from inrnet import args as args_module, jobs, util
 # def test_parser():
 #     args_module.parse_args(['-c','inet_i2', '-j','pytest', '-w'])
 
-def test_get_job_args():
-    args = jobs.get_job_args('manual')
-    assert 'data loading' in args
-    ds = jobs.get_dataset_for_job('manual')
-    assert ds == args["data loading"]["dataset"]
-    jobs.rename_job('manual', 'testmanual')
-    assert jobs.get_job_args('testmanual') == args
-    jobs.rename_job('testmanual', 'manual')
-
 # def test_meshgrid():
 #     assert util.meshgrid(torch.arange(2), torch.arange(2))
